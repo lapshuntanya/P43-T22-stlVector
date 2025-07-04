8d35f86c478640b53feca6a10246c9e2cff7de45
Tetiana Lapshun <lapshun.tatiana@gmail.com>
2025-07-04 10:18:03 +0300

    vector

--- main.cpp
+++ main.cpp
@@ -3,8 +3,16 @@
 int main() {
 
     Vector<int> a; //size = 0, arr = nullptr
+    a.push_back(3);
+    a.push_back(9);
+    a.push_back(-67);
+
     Vector<int> b(6, 0); //size = 6, [0 0 0 0 0 0]
+    b.push_back(89);
+
     Vector<char> k(3, 'a'); // size = 3, [a a a]
+    k.push_back('d');
+    k.push_back('!');
     //Створіть масив дробових чисел із 7 елементів зі значенням 3.14
 
     a.print();
